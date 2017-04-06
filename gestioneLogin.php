70c0689c272f2424feb3e3e9e4a254aa91d6f620
BlueStar <pertile.giulio@gmail.com>
2017-04-06 09:56:18 +0000

    Login created

--- gestioneLogin.php
+++ gestioneLogin.php
@@ -1,3 +1,36 @@
 <?php
-
+    $host="localhost";
+    $username="root";
+    $password="";
+    $dbname="biblio";
+    
+    $conn = new mysqli($host, $username, $password, $dbname);
+    
+    if(!($conn->connect_error))
+    {
+        $userLog=$_POST["user"];
+        $pwLog=$_POST["pw"];
+        
+        $query = "SELECT username, password, permessi, email 
+                    FROM Account 
+                    WHERE username='" . $userLog . "' OR email='" . $userLog . "';";
+        
+        $res=$conn->query($query);
+        
+        if($row = $res->fetch_assoc())
+        {
+            if(((strcmp($row["username"], $userLog)==0)&&(strcmp($row["password"], $pwLog)==0))OR((strcmp($row["email"], $userLog)==0)&&(strcmp($row["password"], $pwLog)==0)))
+            {
+                session_start();
+                $_SESSION["user"]=$row["username"];
+                $_SESSION["permit"]=$row["permessi"];
+                
+                if(strcmp($_SERVER["permit"], 'A')==0)
+                    header("location: alunno.php");
+                else
+                    header("location: gestore.php");
+                die();
+            }
+        }
+    }
 ?>
